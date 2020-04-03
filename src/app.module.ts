046bde9e7ea05e76fd910efcfb6d44c436f0d370
chingaloEric <echingalo@gmail.com>
2020-04-03 19:56:05 +0300

    feat: 🎸 added database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MichangoModule } from './modules/michango/michango.module';
 import { KitengoModule } from './modules/kitengo/kitengo.module';
+import { LoggingInterceptor } from './shared/interceptors/logging.interceptor';
+import { HttpExceptionFilter } from './shared/filters/http-exception.filters';
 
 @Module({
-  imports: [MichangoModule, KitengoModule],
+  imports: [TypeOrmModule.forRoot(), MichangoModule, KitengoModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    { provide: APP_FILTER, useClass: HttpExceptionFilter },
+    { provide: APP_INTERCEPTOR, useClass: LoggingInterceptor },
+  ],
 })
 export class AppModule {}
