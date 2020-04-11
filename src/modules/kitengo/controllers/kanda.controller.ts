34b3c844686e874a95701c8f1b5f682563558eca
chingaloEric <echingalo@gmail.com>
2020-04-11 20:00:11 +0300

    feat: 🎸 added controllers for the entities

---
+++ src/modules/kitengo/controllers/kanda.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Post, Body, UsePipes } from '@nestjs/common';
+
+import { BaseController } from 'src/shared/controllers/base.controller';
+import { Kanda } from '../entities/kanda.entity';
+import { KandaDTO } from '../dtos/kanda.dto';
+import { KandaService } from '../services/kanda.service';
+
+@Controller('kanda')
+export class KandaController extends BaseController<Kanda, KandaDTO> {
+  constructor(private readonly kandaService: KandaService) {
+    super(kandaService);
+  }
+}
--- src/modules/kitengo/kitengo.module.ts
+++ src/modules/kitengo/kitengo.module.ts
@@ -4,9 +4,12 @@ import { Jumuhiya } from './entities/jumuhiya.entity';
 import { Kanda } from './entities/kanda.entity';
 import { KandaService } from './services/kanda.service';
 import { JumuhiyaService } from './services/jumuhiya.service';
+import { JumuhiyaController } from './controllers/jumuhiya.controller';
+import { KandaController } from './controllers/kanda.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Jumuhiya, Kanda])],
   providers: [KandaService, JumuhiyaService],
+  controllers: [JumuhiyaController, KandaController],
 })
 export class KitengoModule {}
