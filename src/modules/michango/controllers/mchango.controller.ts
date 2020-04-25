4f1831390c439e422cd676235129c4b8d6fa807b
chingaloEric <echingalo@gmail.com>
2020-04-25 15:15:19 +0300

    style: 💄 removed unnecessary imports
    Closes: #11

--- src/modules/michango/controllers/mchango.controller.ts
+++ src/modules/michango/controllers/mchango.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, UsePipes } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 
 import { BaseController } from 'src/shared/controllers/base.controller';
 import { Mchango } from 'src/modules/michango/entities/mchango.entity';
