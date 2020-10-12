99a0ed3034bb661e747c540f16d0eb971de28454
chingaloEric <echingalo@gmail.com>
2020-10-12 10:57:50 +0300

    docs: ✏️ registered api properties in dtos

--- src/modules/michango/dtos/mchango.dto.ts
+++ src/modules/michango/dtos/mchango.dto.ts
@@ -1,17 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString, IsInt } from 'class-validator';
 
 export class MchangoDTO {
+  @ApiProperty()
   @IsNotEmpty()
   @IsInt()
   amount: number;
 
+  @ApiProperty()
   @IsInt()
   mass?: number;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   kanda: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   jumuhiya: string;
