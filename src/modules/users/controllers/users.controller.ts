640ee238aa9e8d0069a06d8485f2293ffeebd18a
chingaloEric <echingalo@gmail.com>
2020-10-12 12:14:57 +0300

    refactor: 💡 removed unneeded endoint
    removed the user register endpoint

--- src/modules/users/controllers/users.controller.ts
+++ src/modules/users/controllers/users.controller.ts
@@ -1,24 +1,15 @@
-import { Controller, Post, UsePipes, Body, UseGuards } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 
 import { BaseController } from 'src/shared/controllers/base.controller';
 import { Users } from '../entities/users.entity';
 import { UserDTO } from '../dtos/user.dto';
 import { UsersService } from '../services/users.service';
-import { CustomValidationPipe } from 'src/shared/pipes/validation.pipe';
-import { SessionGuard } from '../guards/session.guard';
-import { ApiTags } from '@nestjs/swagger';
 
 @ApiTags('users')
 @Controller('users')
 export class UsersController extends BaseController<Users, UserDTO> {
   constructor(private readonly usersService: UsersService) {
     super(usersService);
-  }
-
-  @Post('register')
-  @UseGuards(SessionGuard)
-  @UsePipes(new CustomValidationPipe())
-  async register(@Body() user: UserDTO) {
-    return await this.usersService.create(user);
   }
 }
